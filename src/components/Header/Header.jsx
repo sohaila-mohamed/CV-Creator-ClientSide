b56be807eddbe724dcaf9f295f912c62d6da8fd3
sohila-mohamed <sohailamohamed.sbme@gmail.com>
2021-06-13 20:48:23 +0200

    responsive header with medium size bug fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav,Button } from 'react-bootstrap'
 import logo from '../../images/logo.svg'
 import './Header.css';
 function Header() {
@@ -17,28 +17,32 @@ function Header() {
 
                     </div>
                     <div className="col-2 offset-4 d-lg-none d-flex align-content-center justify-content-center">
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     </div>
                     <div className="col-lg-6 col-12 offset-lg-0 offset-1 d-flex align-content-center ">
-                       
+
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav >
-                                <Nav.Link href="#home">Home</Nav.Link>
-                                <Nav.Link href="#link">Link</Nav.Link>
-                                <Nav.Link href="#home">Home</Nav.Link>
-                                <Nav.Link href="#link">Link</Nav.Link>
-                                <Nav.Link href="#home">Home</Nav.Link>
-
+                                <Nav.Link href="#home" className="pr-4">Home</Nav.Link>
+                                <Nav.Link href="#link" className="pr-4">Link</Nav.Link>
+                                <Nav.Link href="#home" className="pr-4">Home</Nav.Link>
+                                <Nav.Link href="#link" className="pr-4">Link</Nav.Link>
+                                <Nav.Link href="#home" className="pr-4">Home</Nav.Link>
+                                <Nav.Link href="#home" className="pr-4 d-lg-none d-inline-block">Sign-IN</Nav.Link>
+                                <Nav.Link href="#home" className="pr-4 d-lg-none d-inline-block">Sign-UP</Nav.Link>
+                                <Button variant="primary" className="sign-btn d-lg-inline-block d-none">Sign-IN</Button>
+                                <Button  className="sign-btn d-lg-inline-block d-none" id="outLined-btn">Sign-UP</Button>
+                            
                             </Nav>
                         </Navbar.Collapse>
 
                     </div>
-                    
-                    
-                   
+
+
+
                 </div>
             </div>
-          
+
 
 
         </Navbar>
