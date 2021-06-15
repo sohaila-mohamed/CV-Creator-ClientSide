78610bd62db2b3f18c32e709490f68826f4e4333
sohila-mohamed <sohailamohamed.sbme@gmail.com>
2021-06-15 20:07:06 +0200

    small logo change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,95 +4,95 @@ import { NavLink } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
-  return (
-    <Navbar className="shadow" bg="white" expand="lg">
-      <div className="container-fluid">
-        <div className="row m-0 p-0 justify-content-start w-100">
-          <div className="col-3">
-            <Navbar.Brand href="#home" className="m-lg-2">
-              <img
-                src={logo}
-                className="d-inline-block align-center "
-                id="Logo"
-              />
-            </Navbar.Brand>
-          </div>
-          <div className="col-2 offset-7 d-lg-none d-flex align-content-center justify-content-end">
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          </div>
-          <div className="col-lg-6 col-12 d-flex align-items-center ">
-            <Navbar.Collapse id="basic-navbar-nav ">
-              <Nav className="d-flex align-content-center  m-auto ">
-                <NavLink
-                  exact
-                  to="/"
-                  activeClassName="Active-link"
-                  className="pr-3  nav-link position-relative"
-                >
-                  Home
+    return (
+        <Navbar className="shadow" bg="white" expand="lg">
+            <div className="container-fluid">
+                <div className="row m-0 p-0 justify-content-start w-100">
+                    <div className="col-3">
+                        <Navbar.Brand href="#home" className="m-lg-2">
+                            <img
+                                src={logo}
+                                className="d-inline-block align-center "
+                                id="Logo"
+                            />
+                        </Navbar.Brand>
+                    </div>
+                    <div className="col-2 offset-7 d-lg-none d-flex align-content-center justify-content-end">
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    </div>
+                    <div className="col-lg-6 col-12 d-flex align-items-center ">
+                        <Navbar.Collapse id="basic-navbar-nav ">
+                            <Nav className="d-flex align-content-center  m-auto ">
+                                <NavLink
+                                    exact
+                                    to="/"
+                                    activeClassName="Active-link"
+                                    className="pr-3  nav-link position-relative"
+                                >
+                                    Home
                 </NavLink>
-                <NavLink
-                  to="/Editor"
-                  activeClassName="Active-link"
-                  className="pr-3 nav-link position-relative"
-                >
-                  Resume Builder
+                                <NavLink
+                                    to="/Editor"
+                                    activeClassName="Active-link"
+                                    className="pr-3 nav-link position-relative"
+                                >
+                                    Resume Builder
                 </NavLink>
-                <NavLink
-                  to="/templates"
-                  activeClassName="Active-link"
-                  className="pr-3 nav-link position-relative"
-                >
-                  Templates
+                                <NavLink
+                                    to="/templates"
+                                    activeClassName="Active-link"
+                                    className="pr-3 nav-link position-relative"
+                                >
+                                    Templates
                 </NavLink>
-                <NavLink
-                  to="/tips"
-                  activeClassName="Active-link"
-                  className="pr-3 nav-link position-relative"
-                >
-                  Tips
+                                <NavLink
+                                    to="/tips"
+                                    activeClassName="Active-link"
+                                    className="pr-3 nav-link position-relative"
+                                >
+                                    Tips
                 </NavLink>
-                <NavLink
-                  to="/about-us"
-                  activeClassName="Active-link"
-                  className="pr-3 nav-link position-relative"
-                >
-                  About-us
+                                <NavLink
+                                    to="/about-us"
+                                    activeClassName="Active-link"
+                                    className="pr-3 nav-link position-relative"
+                                >
+                                    About-us
                 </NavLink>
-                <NavLink
-                  to="/sign-in"
-                  className="pr-3 d-lg-none nav-link d-inline-block"
-                >
-                  Sign-IN
+                                <NavLink
+                                    to="/sign-in"
+                                    className="pr-3 d-lg-none nav-link d-inline-block"
+                                >
+                                    Sign-IN
                 </NavLink>
-                <NavLink
-                  to="/sign-up"
-                  className="pr-3 d-lg-none nav-link d-inline-block"
-                >
-                  Sign-UP
+                                <NavLink
+                                    to="/sign-up"
+                                    className="pr-3 d-lg-none nav-link d-inline-block"
+                                >
+                                    Sign-UP
                 </NavLink>
-              </Nav>
-            </Navbar.Collapse>
-          </div>
-          <div className="col-3  d-none d-lg-flex align-items-center justify-content-end">
-            <Button
-              variant="primary"
-              className="sign-btn d-lg-inline-block d-none"
-              id="filled-btn"
-            >
-              Sign-IN
+                            </Nav>
+                        </Navbar.Collapse>
+                    </div>
+                    <div className="col-3  d-none d-lg-flex align-items-center justify-content-end">
+                        <Button
+                            variant="primary"
+                            className="sign-btn d-lg-inline-block d-none"
+                            id="filled-btn"
+                        >
+                            Sign-IN
             </Button>
-            <Button
-              className="sign-btn d-lg-inline-block d-none"
-              id="outLined-btn"
-            >
-              Sign-UP
+                        <Button
+                            className="sign-btn d-lg-inline-block d-none"
+                            id="outLined-btn"
+                        >
+                            Sign-UP
             </Button>
-          </div>
-        </div>
-      </div>
-    </Navbar>
-  );
+                    </div>
+                </div>
+            </div>
+        </Navbar>
+    );
 }
 
 export default Header;
