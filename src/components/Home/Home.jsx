0b57c69b3b6d7bae41054475220379aa760d2f3e
sohila-mohamed <sohailamohamed.sbme@gmail.com>
2021-06-16 17:29:41 +0200

    conveerting description section to array

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,8 +7,17 @@ import "./Home.css";
 import verified from "../../images/verified.svg";
 import descImg_1 from '../../images/description/description-img1.png';
 import number_01 from '../../images/description/01.png';
+import number_02 from '../../images/description/02.svg';
+import number_03 from '../../images/description/03.svg';
 
 function Home() {
+  var description_items=[{image:number_01,title:" Register in a Flash",text:` It only takes a couple of seconds to start using our online resum builder.
+  The resume creator is offered completely through our website, meaning
+  there is no software to download.`},{image:number_02,title:"Register in a Flash",text:` It only takes a couple of seconds to start using our online resum builder.
+  The resume creator is offered completely through our website, meaning
+  there is no software to download.`},{image:number_03,title:"Register in a Flash",text:` It only takes a couple of seconds to start using our online resum builder.
+  The resume creator is offered completely through our website, meaning
+  there is no software to download.`}];
   return (
     <Fragment>
       <div className="card position-relative border-0 mb-3">
@@ -59,6 +68,46 @@ function Home() {
         </div>
       </div>
       <div className="home-desc-section">
+      <Container className="p-0 home-desc-container">
+      <div className="row m-0">
+    <div className="col-8 ml-1 ml-sm-0 d-flex">
+      <span className="home-desc-header">How to make a resume?</span>
+    </div>
+  </div>
+        {
+description_items.map((item)=>(
+  
+ <div >
+  <div className="row m-0 p-0">
+    <div className="col-12  m-0 position-relative  ">
+      <img src={item.image} className="home-desc-numbers "/>
+    </div>
+  </div>
+  <div className="row m-0 home-desc-line position-relative">
+    <div className="col-lg-8 col-md-8 col-sm-8 col-12 home-desc-sideText ">
+      <h1 >
+        {item.title}
+    </h1>
+      <span>
+       {item.text}
+    </span>
+
+    </div>
+    <div className="col-lg-4 col-md-4 col-sm-4 col-8 offset-2 offset-lg-0 offset-md-0  offset-sm-0 d-flex">
+      <img src={descImg_1} className="home-desc-sideImg align-self-end"></img>
+
+    </div>
+  </div>
+  </div>
+
+
+))
+        }
+        </Container>
+         </div>
+      
+      
+      {/* <div className="home-desc-section">
         <Container className="p-0 home-desc-container">
           <div className="row m-0">
             <div className="col-8 ml-1 ml-sm-0 d-flex">
@@ -88,7 +137,7 @@ function Home() {
             </div>
           </div>
         </Container>
-      </div>
+      </div> */}
     </Fragment>
   );
 }
