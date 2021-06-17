d0d75b19b4d4589b0722af02593a3786236f6bb4
unknown <maiarmostafa98@gmail.com>
2021-06-18 00:55:05 +0200

    making plan responsive

--- src/components/Home/Plans/Plan.jsx
+++ src/components/Home/Plans/Plan.jsx
@@ -8,11 +8,11 @@ function Plan() {
           Our plans
         </p>
         <div className="d-flex justify-content-around flex-wrap">
-          <div className="card p-3 w-25 shadow mb-5">
+          <div className="card mb-5 p-3 shadow mb-5">
             <p className="Plan-color-gray p-3 text-center">Service #1</p>
             <div className="card-body">
               <p className="text-price">Free</p>
-              <p className="text-dark text-center">
+              <p className="text-dark text-center card-text">
                 You can create up to 2 CVs with all of the available Categories
                 and Templates
               </p>
@@ -22,7 +22,7 @@ function Plan() {
             </div>
           </div>
 
-          <div className="card p-3 w-25 shadow mb-5">
+          <div className="card mb-5 p-3 shadow mb-5">
             <p className="Plan-color-blue p-3 text-center">Service #2</p>
             <div className="card-body">
               <p className="text-price m-0">
@@ -31,7 +31,7 @@ function Plan() {
               <p className="text-center small p-0" style={{ color: "#C0C0C0" }}>
                 Per month
               </p>
-              <p className="text-dark text-center">
+              <p className="text-dark text-center card-text">
                 You can create up to 10 CVs with all of the available Categories
                 and Tempaltes
               </p>
@@ -41,7 +41,7 @@ function Plan() {
             </div>
           </div>
 
-          <div className="card p-3 w-25 shadow mb-5">
+          <div className="card mb-5 p-3 shadow mb-5">
             <p className="Plan-color-gray p-3 text-center">Service #3</p>
             <div className="card-body">
               <p className="text-price m-0">
@@ -50,7 +50,7 @@ function Plan() {
               <p className="text-center small p-0" style={{ color: "#C0C0C0" }}>
                 Per month
               </p>
-              <p className="text-dark text-center">
+              <p className="text-dark text-center card-text">
                 You can create up to 20 CVs with all of the available Categories
                 and Tempaltes
               </p>
