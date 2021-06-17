12219d4ad1a7157399f95c4ca649d6c90905e182
unknown <maiarmostafa98@gmail.com>
2021-06-17 16:23:05 +0200

    edit footer im md screen and make it responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,16 +9,16 @@ import { NavLink } from "react-router-dom";
 function Footer() {
   return (
     <div id="Footer" className="p-2">
-      <div className="container-fluid p-0 d-flex flex-wrap justify-content-between h-100 w-100">
-        <div className="mb-3 d-inline-block ml-5 align-self-center text-sm-center text-center text-lg-left text-md-left flex-lg-grow-0 flex-md-grow-0 flex-sm-grow-0 flex-grow-1">
+      <div className="container-fluid p-0 d-lg-flex d-md-flex d-block flex-wrap justify-content-between h-100 w-100">
+        <div className="mb-3 d-lg-inline-block d-md-inline-block d-sm-block d-block ml-lg-5 ml-md-5 ml-0 align-self-center text-sm-center text-center text-lg-left text-md-left">
           <img src={logo} alt="Logo" id="Footer-logo" />
           <img
             src={Line}
             alt="Line after logo"
-            className="ml-3 d-lg-inline-block d-md-inline-block d-sm-inline-block d-none"
+            className="ml-3 d-lg-inline-block d-md-inline-block d-sm-none d-none"
           />
         </div>
-        <div className="flex-grow-1 text-sm-center text-center text-lg-left text-md-left d-inline-block align-self-center">
+        <div className="flex-grow-1 text-sm-center text-center text-lg-left text-md-left d-lg-inline-block d-md-inline-block d-sm-block d-block align-self-center">
           <NavLink to="/" href="#" className="Footer-a ml-4">
             Plans
           </NavLink>
@@ -33,7 +33,7 @@ function Footer() {
           </NavLink>
           <p className="p-0 ml-4">&copy; 2021 ITI-Team. All Rights Reserved.</p>
         </div>
-        <div className="d-inline-block align-self-center ml-5 mr-5 text-sm-center text-center text-lg-left text-md-left flex-lg-grow-0 flex-md-grow-0 flex-sm-grow-1 flex-grow-1">
+        <div className="d-lg-inline-block d-md-inline-block d-sm-block d-block align-self-center ml-5 mr-5 text-sm-center text-center text-lg-left text-md-left">
           <img src={facebook} alt="facbook" className="icon mr-3" />
           <img src={Linkedin} alt="linkedin" className="icon mr-3" />
           <img src={twitter} alt="twitter" className="icon mr-3" />
