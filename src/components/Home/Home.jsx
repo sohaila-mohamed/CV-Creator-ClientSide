79e5ebce739a2fdd9bc2512fd2ef8ba81688ab76
sohila-mohamed <sohailamohamed.sbme@gmail.com>
2021-06-17 15:17:59 +0200

    separating text from structure

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,15 +9,20 @@ import descImg_1 from '../../images/description/description-img1.png';
 import number_01 from '../../images/description/01.png';
 import number_02 from '../../images/description/02.svg';
 import number_03 from '../../images/description/03.svg';
+import number_04 from '../../images/description/04.svg';
+import './description';
+import { registration_head, registration_text, categories_text, categories_head, content_head, content_text, export_head, export_text } from "./description";
 
 function Home() {
-  var description_items=[{image:number_01,title:" Register in a Flash",text:` It only takes a couple of seconds to start using our online resum builder.
-  The resume creator is offered completely through our website, meaning
-  there is no software to download.`},{image:number_02,title:"Register in a Flash",text:` It only takes a couple of seconds to start using our online resum builder.
-  The resume creator is offered completely through our website, meaning
-  there is no software to download.`},{image:number_03,title:"Register in a Flash",text:` It only takes a couple of seconds to start using our online resum builder.
-  The resume creator is offered completely through our website, meaning
-  there is no software to download.`}];
+
+  var description_items = [
+  { image: number_01, title: registration_head, text: registration_text },
+  { image: number_02, title: categories_head, text: categories_text },
+  { image: number_03, title: content_head, text: content_text },
+  { image: number_04, title: export_head, text: export_text },
+  ];
+
+
   return (
     <Fragment>
       <div className="card position-relative border-0 mb-3">
@@ -67,77 +72,47 @@ function Home() {
           </div>
         </div>
       </div>
+
+
+
       <div className="home-desc-section">
-      <Container className="p-0 home-desc-container">
-      <div className="row m-0">
-    <div className="col-8 ml-1 ml-sm-0 d-flex">
-      <span className="home-desc-header">How to make a resume?</span>
-    </div>
-  </div>
-        {
-description_items.map((item)=>(
-  
- <div >
-  <div className="row m-0 p-0">
-    <div className="col-12  m-0 position-relative  ">
-      <img src={item.image} className="home-desc-numbers "/>
-    </div>
-  </div>
-  <div className="row m-0 home-desc-line position-relative">
-    <div className="col-lg-8 col-md-8 col-sm-8 col-12 home-desc-sideText ">
-      <h1 >
-        {item.title}
-    </h1>
-      <span>
-       {item.text}
-    </span>
-
-    </div>
-    <div className="col-lg-4 col-md-4 col-sm-4 col-8 offset-2 offset-lg-0 offset-md-0  offset-sm-0 d-flex">
-      <img src={descImg_1} className="home-desc-sideImg align-self-end"></img>
-
-    </div>
-  </div>
-  </div>
-
-
-))
-        }
-        </Container>
-         </div>
-      
-      
-      {/* <div className="home-desc-section">
         <Container className="p-0 home-desc-container">
           <div className="row m-0">
             <div className="col-8 ml-1 ml-sm-0 d-flex">
               <span className="home-desc-header">How to make a resume?</span>
             </div>
           </div>
-          <div className="row m-0 p-0">
-            <div className="col-12  m-0 position-relative ">
-              <img src={number_01} className="home-desc-numbers "/>
-            </div>
-          </div>
-          <div className="row m-0 home-desc-line position-relative">
-            <div className="col-lg-8 col-md-8 col-sm-8 col-12 home-desc-sideText ">
-              <h1 >
-                Register in a Flash
-            </h1>
-              <span>
-                It only takes a couple of seconds to start using our online resum builder.
-    The resume creator is offered completely through our website, meaning
-     there is no software to download.
-            </span>
+          {
+            description_items.map((item) => (
 
-            </div>
-            <div className="col-lg-4 col-md-4 col-sm-4 col-8 offset-2 offset-lg-0 offset-md-0  offset-sm-0 d-flex">
-              <img src={descImg_1} className="home-desc-sideImg align-self-end"></img>
+              <div >
+                <div className="row m-0 p-0">
+                  <div className="col-12  m-0 position-relative  ">
+                    <img src={item.image} className="home-desc-numbers " />
+                  </div>
+                </div>
+                <div className="row m-0 home-desc-line position-relative">
+                  <div className="col-lg-8 col-md-8 col-sm-8 col-12 home-desc-sideText ">
+                    <h1 >
+                      {item.title}
+                    </h1>
+                    <span>
+                      {item.text}
+                    </span>
 
-            </div>
-          </div>
+                  </div>
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-8 offset-2 offset-lg-0 offset-md-0  offset-sm-0 d-flex">
+                    <img src={descImg_1} className="home-desc-sideImg align-self-end"></img>
+
+                  </div>
+                </div>
+              </div>
+
+
+            ))
+          }
         </Container>
-      </div> */}
+      </div>
     </Fragment>
   );
 }
---
+++ src/components/Home/description.js
@@ -0,0 +1,34 @@
+const registration_head = " registration_text"
+const registration_text = `It only takes a couple of seconds to start using our online resum builder. 
+The resume creator is offered completely through our website, meaning
+ there is no software to download.`
+
+
+const categories_head = `Open categories section`;
+const categories_text = `First open cvcreator then click on catiegories and choose the best templete, 
+Click on it an start to fill the dataalso feel free to add section or to remove 
+section.`
+
+const content_head = "Develop and Optimize Your Content";
+const content_text = `Each resume template is organized into major content sections 
+that you fill in while optimization tools guide you through the
+process. And adding or removing a specific section based on 
+your needs is no problem and you get layout and content 
+suggestions so that your resume looks perfect.`;
+
+const export_head = `Export and Send`;
+const export_text = `Once your content is finished, you can export your resume in PDF from
+the free resume builder. Your latest version is saved and you can
+always go back to make edits.`;
+
+export {
+    registration_head,
+    registration_text,
+    categories_head,
+    categories_text,
+    content_head,
+    content_text,
+    export_head,
+    export_text
+
+}
