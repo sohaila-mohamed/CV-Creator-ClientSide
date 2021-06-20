3c2340dde62af8492a1867c369ab213937fdc147
omarahmedfarag <omarfarag429@gmail.com>
2021-06-20 23:39:41 +0200

    Editor statful components

--- src/components/editor/editorWrapper/editorWrapper.jsx
+++ src/components/editor/editorWrapper/editorWrapper.jsx
@@ -1,30 +1,44 @@
 import { faEye } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import CvD1 from '../CvTemplate/CvD1'
 import Education from '../EditorComponents/Education/Education'
 import PersonalInfo from '../EditorComponents/PersonalInfo/PersonalInfo'
 import "./editorWrapper.css"
 import Navigator from './Navigator/Navigator'
-function editorWrapper() {
+function EditorWrapper() {
+
+    const [personalInfo, setpersonalInfo] = useState({firstName:"",lastName:"",age:"",phoneNumber:""})
+
+    
+    const [educationData, seteducationData] = useState({university:"",graduationYear:"",grade:""})
+
+    const personalHandler = (data)=>{
+        setpersonalInfo(data)
+    }
+    const educationHandler = (data)=>{
+        seteducationData(data)
+    }
+
     return (
         <div className="editorWrapper-container">
-            <div className="left-editor">
+            <div className="left-editor w-100 w-md-50">
                 <Navigator/>
 
                 {/* to include each editor component here Ex education component , personal information component , ect */}
                 <div className="each-form-component">
                
                 <Switch>
-                    <Route path="/Editor/PersonalInfo" component={PersonalInfo} />
+                    <Route path="/Editor/PersonalInfo" render={()=> <PersonalInfo data={personalInfo} setPersonalData={personalHandler}></PersonalInfo>}/>
+                    <Route path="/Editor/Education" render={()=> <Education data={educationData} setEducationData={educationHandler}></Education>}/>
                     <Route path="/Editor/Education" component={Education} />
                     <Route path="/Editor/Contacts" render={()=><p>Contacts</p>} />
                     <Route path="/Editor/Experiences" render={()=><h3>Experiences</h3>} />
                     <Route path="/Editor/Skilles" render={()=><h3>Skilles</h3>} />
                     <Route path="/Editor/Courses" render={()=><h3>Courses</h3>} />
                     <Route path="/Editor/Media-accounts" render={()=><h3>Media-accounts</h3>} />
-                    <Route path="/Editor/About-me" render={()=><h3>About-me</h3>} />
+                    <Route path="/Editor/About-me" render={()=><h3>About me</h3>} />
                    
                 </Switch>
                
@@ -32,7 +46,7 @@ function editorWrapper() {
 
             </div>
 
-            <div className="right-editor">
+            <div className="right-editor d-none d-md-block w-100 w-md-50">
                 <button>
                 <FontAwesomeIcon icon={faEye}  className="mr-2"/>
                     Preview
@@ -46,4 +60,4 @@ function editorWrapper() {
     )
 }
 
-export default editorWrapper
+export default EditorWrapper
